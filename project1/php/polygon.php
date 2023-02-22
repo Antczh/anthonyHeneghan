ca559d86a409509f667c0ac55db6c27b52ee362d
Antczh <ant.c.h@hotmail.co.uk>
2023-02-22 23:59:38 +0000

    code tidy up

--- project1/php/polygon.php
+++ project1/php/polygon.php
@@ -9,20 +9,6 @@ $output['status']['name'] = "ok";
 $output['status']['description'] = "success";
 
 $countryCode = $_GET['c'];
-// if(code == geometry) {
-
-// }
-
-// $countries = [];
-
-// foreach ($decode["features"] as $feature) {
-//     $country = [
-//         "name" => $feature["properties"]["name"],
-//         "code" => $feature["properties"]["iso_a2"]
-//     ];
-//     $countries[] = $country;
-// }
-// $output['countries'] = $countries;
 
 
 $searchedCountry = NULL;
@@ -32,42 +18,5 @@ foreach ($decode['features'] as $feature) {
     }
 }
 
-// foreach ($decode['features'] as $feature) {
-
-//     $country_geometry = [
-//         "code" => $feature['properties']['iso_a2'],
-//         "geometry" => $feature['geometry']['coordinates'],
-//     ];
-//     $country_geometries[] = $country_geometry;
-// }
-
-// $output['country_geometries'] = $country_geometries;
 
 echo json_encode($searchedCountry);
-
-// ---------------------------------------------instructions--------------------------------------
-// get country code from url like get latlng.php
-// loop thorugh countries from json file like request.php
-// loop need if statement need to checks if code matches
-// get geometry data 
-// in js 
-
-// ---------------------------------------------instructions--------------------------------------
-
-
-
-
-
-
-
-// -----------------------------------This works-----------------------------------------------
-// $country_geometries = [];
-// foreach ($decode['features'] as $feature) {
-//     $country_code = $feature['properties']['iso_a2'];
-//     $country_geometry = $feature['geometry']['coordinates'];
-//     $country_geometries[$country_code] = $country_geometry;
-// }
-
-// $output['country_geometries'] = $country_geometries;
-
-// -----------------------------------This works-----------------------------------------------

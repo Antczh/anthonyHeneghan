5266a4e4e03d8faeaec7bdbcdedacae04e8e0aa1
Antczh <ant.c.h@hotmail.co.uk>
2023-03-23 20:46:07 +0000

    filtering through personnel cards

--- project2/libs/js/script.js
+++ project2/libs/js/script.js
@@ -8,7 +8,7 @@
 
 //       const personnelCard = $("#personnelCard");
 
-//       let employeeInfo = "";
+//       personnelCard.empty(); // Clear any existing cards before appending new ones
 
 //       for (let i = 0; i < res.data.length; i++) {
 //         const item = res.data[i];
@@ -18,34 +18,39 @@
 //         const employeeDepartment = item.department;
 //         const employeeLocation = item.location;
 
-//         employeeInfo += `<div class="employeeCard card mb-3">
-//             <div class="row g-0">
-//               <div class="col-md-4">
-//                 <img
-//                   src="./fontawesome-free-6.3.0-web/svgs/solid/id-badge-solid.svg"
-//                   class="img-fluid rounded-start"
-//                   alt="Personnel Profile Photo"
-//                 />
-//               </div>
-//               <div class="col-md-8">
-//                 <div class="card-body">
-//                   <h3 class="card-title" id="personnelName">${employeeFirstName} ${employeeLastName}</h3>
-//                   <h6 id="personnelEmail">${employeeEmail}</h6>
-//                   <h6 id="personnelDepartment">${employeeDepartment}</h6>
-//                   <h6 id="personnelLocation">${employeeLocation}</h6>
-//                 </div>
+//         const employeeCardHTML = `
+//           <div class="col-sm-4 mb-3">
+//             <div class="card">
+//               <div class="card-body">
+//               <img src="./fontawesome-free-6.3.0-web/svgs/solid/id-badge-solid.svg" class="img-fluid rounded-start" alt="Personnel Profile Photo" style="width: 100px; height: 140px;" />
+
+//                 <h5 class="card-title">${employeeFirstName} ${employeeLastName}</h5>
+//                 <p class="card-text">${employeeEmail}</p>
+//                 <p class="card-text"> ${employeeDepartment}</p>
+//                 <p class="card-text"> ${employeeLocation}</p>
+//                 <a href="#" class="btn btn-warning">Edit</a> <a href="#" class="btn btn-danger">Delete</a>
+
 //               </div>
 //             </div>
 //           </div>
 //         `;
-//       }
 
-//       personnelCard.append(employeeInfo);
+//         personnelCard.append(employeeCardHTML);
+//       }
 //     },
 //   });
 // }
 
-function loadEmployeeInfo() {
+// $(document).ready(function () {
+//   loadEmployeeInfo();
+// });
+
+// $("#homeBtn").click(function () {
+//   console.log("home button clicked...");
+//   loadEmployeeInfo();
+// });
+
+function loadEmployeeInfo(filter = "") {
   $.ajax({
     url: "libs/php/getAll.php",
     type: "GET",
@@ -54,11 +59,15 @@ function loadEmployeeInfo() {
       console.log(res);
 
       const personnelCard = $("#personnelCard");
-
       personnelCard.empty(); // Clear any existing cards before appending new ones
 
-      for (let i = 0; i < res.data.length; i++) {
-        const item = res.data[i];
+      const filteredData = res.data.filter((item) =>
+        `${item.firstName} ${item.lastName}`
+          .toLowerCase()
+          .includes(filter.toLowerCase())
+      );
+
+      filteredData.forEach((item) => {
         const employeeFirstName = item.firstName;
         const employeeLastName = item.lastName;
         const employeeEmail = item.email;
@@ -70,7 +79,7 @@ function loadEmployeeInfo() {
             <div class="card">
               <div class="card-body">
               <img src="./fontawesome-free-6.3.0-web/svgs/solid/id-badge-solid.svg" class="img-fluid rounded-start" alt="Personnel Profile Photo" style="width: 100px; height: 140px;" />
-                 
+
                 <h5 class="card-title">${employeeFirstName} ${employeeLastName}</h5>
                 <p class="card-text">${employeeEmail}</p>
                 <p class="card-text"> ${employeeDepartment}</p>
@@ -83,83 +92,65 @@ function loadEmployeeInfo() {
         `;
 
         personnelCard.append(employeeCardHTML);
-      }
+      });
     },
   });
 }
 
 $(document).ready(function () {
-  loadEmployeeInfo();
-});
+  loadEmployeeInfo(); // Load all employee cards on page load
 
-$("#homeBtn").click(function () {
-  console.log("home button clicked...");
-  loadEmployeeInfo();
-});
-
-$(document).ready(function () {
-  loadEmployeeInfo();
-});
-
-$("#homeBtn").click(function () {
-  console.log("home button clicked...");
-  loadEmployeeInfo();
+  // Listen for changes to the search bar and filter the results accordingly
+  $("#searchBar").on("input", function () {
+    const searchStr = $(this).val();
+    loadEmployeeInfo(searchStr);
+  });
 });
 
 const dropdownItems = document.querySelectorAll(".dropdown-item");
 
-dropdownItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    const selectedOption = item.textContent;
-    console.log(`Selected option: ${selectedOption}`);
-    // Do something with the selected option
-  });
-});
-
-// function showAllDepartments() {
-//   $.ajax({
-//     url: "libs/php/getAllDepartments.php",
-//     type: "GET",
-//     dataType: "json",
-//     success: function (res) {
-//       console.log(res);
-//       const departmentCard = $("#departmentCard");
-
-//       let employeeInfo = "";
-
-//       for (let i = 0; i < res.data.length; i++) {
-//         const item = res.data[i];
-//         const employeeFirstName = item.firstName;
-//         const employeeLastName = item.lastName;
-//         const employeeEmail = item.email;
-//         const employeeDepartment = item.department;
-//         const employeeLocation = item.location;
-
-//         employeeInfo += `<div class="employeeCard card mb-3">
-//             <div class="row g-0">
-//               <div class="col-md-4">
-//                 <img
-//                   src="./fontawesome-free-6.3.0-web/svgs/solid/id-badge-solid.svg"
-//                   class="img-fluid rounded-start"
-//                   alt="Personnel Profile Photo"
-//                 />
-//               </div>
-//               <div class="col-md-8">
-//                 <div class="card-body">
-//                   <h3 class="card-title" id="personnelName">${employeeFirstName} ${employeeLastName}</h3>
-//                   <h6 id="personnelEmail">${employeeEmail}</h6>
-//                   <h6 id="personnelDepartment">${employeeDepartment}</h6>
-//                   <h6 id="personnelLocation">${employeeLocation}</h6>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         `;
-//       }
-//     },
+// dropdownItems.forEach((item) => {
+//   item.addEventListener("click", () => {
+//     const selectedOption = item.textContent;
+//     console.log(`Selected option: ${selectedOption}`);
+//     // Do something with the selected option
+//     // showAllDepartments();
 //   });
-// }
-// $("#allDepartmentsOpt").click(function () {
-//   console.log("Departments option clicked...");
-//   showAllDepartments();
 // });
+
+function showAllDepartments() {
+  $.ajax({
+    url: "libs/php/getAllDepartments.php",
+    type: "GET",
+    dataType: "json",
+    success: function (res) {
+      console.log(res);
+      const departmentCard = $("#departmentCard");
+
+      departmentCard.empty(); // Clear any existing cards before appending new ones
+
+      for (let i = 0; i < res.data.length; i++) {
+        const item = res.data[i];
+        const departmentName = item.name;
+
+        const departmentinfo = `<div class="employeeCard card mb-3">
+        <div class="row g-0">
+          <div class="col-md-4">
+          <div class="col-md-8">
+            <div class="card-body">
+              <h3 class="card-title">${departmentName}</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+
+        departmentCard.append(departmentinfo);
+      }
+    },
+  });
+}
+$("#allDepartmentsOpt").click(function () {
+  console.log("Departments option clicked...");
+  showAllDepartments();
+});

b3841ab9c3b6529ba59691417da68315a53003ea
Antczh <ant.c.h@hotmail.co.uk>
2023-04-17 14:55:08 +0100

    test key hideen

---
+++ project1/config.php
@@ -0,0 +1,7 @@
+<?php
+
+$geonames_key = 'antch';
+$rapid_key = '450da59770msh48f578fe4ec52bcp1e3255jsn924835a58ab7';
+$opencage_key = '4ab907fcf8584274bc630ff7769a2037';
+$weather_key = '2cc2e66eaabbee0304c1c626a25344bc';
+
